b1c5c452bf950b839df1148a03c8327ddd19227a
Olamidun <kolapoolamidun@gmail.com>
2022-06-24 13:14:15 +0100

    test: complete other test for get_user_data endpoint

--- tests/test_authentication/test_get_user_data.py
+++ tests/test_authentication/test_get_user_data.py
@@ -6,7 +6,7 @@ class TestGetUserData:
     def test_successful_get_user_data(self, client, user_token):
         """
         
-        Test for successful registration
+        Test for successful retieval of user data
 
         GIVEN: A user tries to get their data
         WHEN: The user hits the endpoint
@@ -18,3 +18,37 @@ class TestGetUserData:
         
         assert response.status_code == 200
         assert isinstance(response_data, dict)
+
+    def test_unsuccessful_get_user_data_invalid_token(self, client, invalid_user_token):
+        """
+        
+        Test for unsuccessful retieval of user data
+
+        GIVEN: A user tries to get their data
+        WHEN: The user hits the endpoint
+        THEN: They should get a success response of status code 401 and an error message
+
+        """
+        response = client.get("/api/v1/users/user", **invalid_user_token)
+        response_data = response.json()
+        
+        assert response.status_code == 401
+        assert response.json()["detail"] == "Given token not valid for any token type"
+        assert response.json()["code"] == "token_not_valid"
+        assert response.json()["messages"][0]["message"] == "Token is invalid or expired"
+
+    def test_unsuccessful_get_user_data_no_token(self, client, user_token):
+        """
+        
+        Test for unsuccessful retieval of user data
+
+        GIVEN: A user tries to get their data
+        WHEN: The user hits the endpoint
+        THEN: They should get a success response of status code 401 and an error message
+
+        """
+        response = client.get("/api/v1/users/user")
+        response_data = response.json()
+        
+        assert response.status_code == 401
+        assert response.json()['detail'] == "Authentication credentials were not provided."
